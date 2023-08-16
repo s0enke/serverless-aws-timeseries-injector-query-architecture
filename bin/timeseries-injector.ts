d6054dcb5d6e55390b087c8a2571fd09b2f4d5a1
Soenke Ruempler <soenke@ruempler.eu>
2023-08-16 13:18:15 +0200

    chore: use simpler deployment
    with cli creds synthesizer

--- bin/timeseries-injector.ts
+++ bin/timeseries-injector.ts
@@ -18,4 +18,5 @@ new TimeseriesInjectorStack(app, 'TimeseriesInjectorStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  synthesizer: new cdk.CliCredentialsStackSynthesizer(),
 });
